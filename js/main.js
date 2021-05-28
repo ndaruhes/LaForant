a9d2c2d156a2412ebe5b2737ea3c7f0c9a398919
ndaru21 <muhammad.ndaru@gmail.com>
2021-05-28 23:36:33 +0700

    continue design home page & responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // NAVBAR
 let navbar = document.querySelector('.navbar')
+let toggleMenu = document.querySelector('.toggle-menu')
+let menu = document.querySelector('.menu')
+let hamburger = document.querySelector('.hamburger')
+let cross = document.querySelector('.cross')
+
 window.onscroll = function(){
     const yScroll = window.scrollY
     if(yScroll > 100){
@@ -8,4 +13,10 @@ window.onscroll = function(){
         navbar.classList.remove('fly-navbar')
     }
 }
-// console.log(window.scrollY)
+
+toggleMenu.onclick = function(){
+    menu.classList.toggle('display-block')
+    navbar.classList.toggle('navbar-active')
+    hamburger.classList.toggle('display-none')
+    cross.classList.toggle('display-block')
+}
